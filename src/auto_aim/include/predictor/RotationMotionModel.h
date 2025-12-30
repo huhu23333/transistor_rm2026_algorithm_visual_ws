5adc2bef7d0209196d01b86bf3a080ffd4840447
huhu233 <925393268@qq.com>
2025-12-30 19:30:17 +0800

    撤回RMM加速度

--- src/auto_aim/include/predictor/RotationMotionModel.h
+++ src/auto_aim/include/predictor/RotationMotionModel.h
@@ -265,8 +265,6 @@ struct RotationMotionState {
     double center_vx;
     double center_vy;
     double center_vz;
-    double center_ax;
-    double center_ay;
     double r_now;
     double r_another;
     double yaw;
@@ -278,7 +276,7 @@ struct RotationMotionState {
 class RotationMotionModel {
 private:
     // 扩展的状态向量： [center_x, center_y, center_z, center_vx, center_vy, center_vz, r]
-    static constexpr int STATE_DIM = 9;  // 从5增加到7
+    static constexpr int STATE_DIM = 7;  // 从5增加到7
     Eigen::MatrixXd P_center_;      // 7x7 协方差矩阵
     Eigen::VectorXd x_center_;      // 7维状态向量
     double lambda_;                 // 遗忘因子
@@ -297,8 +295,6 @@ private:
     double center_vx;
     double center_vy;
     double center_vz;
-    double center_ax;
-    double center_ay;
     double r_now;
     double r_another;
     double center_x;
