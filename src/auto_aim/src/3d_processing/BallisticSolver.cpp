a6459c0e8599cd30296afc9c029c6f82b66623a3
huhu23333 <925393268@qq.com>
2026-01-03 16:29:42 +0800

    save change

--- src/auto_aim/src/3d_processing/BallisticSolver.cpp
+++ src/auto_aim/src/3d_processing/BallisticSolver.cpp
@@ -373,7 +373,7 @@ BallisticInfo BallisticSolver::calcBallisticAngle(float x_camera, float y_camera
     float final_pitch_rad = pitch_info.target_pitch_result_smaller;
 
     result.delta_pitch_rad = final_pitch_rad - cur_pitch;
-    result.delta_yaw_rad = target_delta_yaw + 2.0 * M_PI / 180.0f; // todo
+    result.delta_yaw_rad = target_delta_yaw; // + 2.0 * M_PI / 180.0f; // todo
     result.valid = true;
     return result;
 }
