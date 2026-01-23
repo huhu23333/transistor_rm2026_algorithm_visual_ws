224aacdb860d07e5651e20db93ccbebec910021d
huhu233 <925393268@qq.com>
2026-01-23 16:07:04 +0800

    修改参数，关闭类型锁定

--- src/auto_aim/src/2d_armor_detector/ArmorClassifier.cpp
+++ src/auto_aim/src/2d_armor_detector/ArmorClassifier.cpp
@@ -135,9 +135,9 @@ std::vector<ArmorResult> ArmorClassifier::classify(
         //current_number = 1;
         //classify_confidence = 1.0;
 
-        classify_confidence = 1.0; 
-        current_number = 0; // 固定类型/////////////
-        is_large_probability = 1.0;
+        // classify_confidence = 1.0; 
+        // current_number = 0; // 固定类型/////////////
+        // is_large_probability = 1.0;
 
         bool is_ture_armor = (is_armor_probability >= IS_ARMOR_THRESHOLD) &&
                              (classify_confidence >= CLASSIFY_THRESHOLD);
