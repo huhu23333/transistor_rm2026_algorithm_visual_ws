98950be2fe6882274d2fb5b02a56c6b0772e1097
huhu233 <925393268@qq.com>
2026-01-23 22:13:59 +0800

    增加控制积分值显示

--- src/auto_aim/src/predictor/PredictorMain.cpp
+++ src/auto_aim/src/predictor/PredictorMain.cpp
@@ -92,5 +92,19 @@ PredictorResult PredictorMain::step(std::vector<ArmorResult>& classifyResults, c
     }
     chosen_result.command_pitch = last_pitch_rad_delayed_ + chosen_result.command_delta_pitch * command_picth_kp + pitch_integration; // PI控制
     chosen_result.command_yaw = last_yaw_rad_delayed_ + chosen_result.command_delta_yaw * command_yaw_kp + yaw_integration; // 缓解yaw轴输入数据掉线问题（并不能()）
+
+
+    cv::putText(frame, 
+        "pitch_integration:"+std::to_string(pitch_integration * 180.0 / M_PI), 
+        cv::Point2f(20,900), 
+        cv::FONT_HERSHEY_COMPLEX, 0.7, 
+        cv::Scalar(0, 255, 0), 1, 8, false);
+    cv::putText(frame, 
+        "yaw_integration:"+std::to_string(yaw_integration * 180.0 / M_PI), 
+        cv::Point2f(20,930), 
+        cv::FONT_HERSHEY_COMPLEX, 0.7, 
+        cv::Scalar(0, 255, 0), 1, 8, false);
+
+
     return chosen_result;
 }
