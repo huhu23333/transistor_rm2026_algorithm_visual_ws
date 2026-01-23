3c1a9b93cb7d5bcc62cb10823516ee2a1a5ba4e1
huhu233 <925393268@qq.com>
2026-01-23 16:18:06 +0800

    使用施密特触发器平滑瞄准位置模式切换

--- src/auto_aim/src/predictor/AllPredictor.cpp
+++ src/auto_aim/src/predictor/AllPredictor.cpp
@@ -595,11 +595,12 @@ RMM_fire_result_t AllPredictor::RMM_fire_control(SimpleArmor chosen_armor, Rotat
     }
     RMM_fire_control_data.last_target_yaw = chosen_armor.yaw;
 
-    if (fabs(RMM_state.vyaw) > RMM_fire_control_data.aim_center_vyaw_threshold) {
-        result.aim_center = true;
-    } else {
-        result.aim_center = false;
+    if (fabs(RMM_state.vyaw) > RMM_fire_control_data.aim_center_vyaw_upper_threshold) {
+        RMM_fire_control_data.aim_center_schmitt_trigger = true;
+    } else if (fabs(RMM_state.vyaw) < RMM_fire_control_data.aim_center_vyaw_lower_threshold) {
+        RMM_fire_control_data.aim_center_schmitt_trigger = false;
     }
+    result.aim_center = RMM_fire_control_data.aim_center_schmitt_trigger;
 
     if (result.aim_center) {
         float max_yaw_bias = std::atan2((is_large_armor ? ArmorConstants::LARGE_ARMOR_WIDTH : ArmorConstants::SMALL_ARMOR_WIDTH) / 2.0, 
