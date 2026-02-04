1bd66c4b1e11697e5a062da68376f4648e85d4fa
huhu233 <925393268@qq.com>
2026-02-04 18:30:01 +0800

    增加目标yaw值初始偏置

--- src/auto_aim/src/predictor/PredictorMain.cpp
+++ src/auto_aim/src/predictor/PredictorMain.cpp
@@ -97,7 +97,7 @@ PredictorResult PredictorMain::step(std::vector<ArmorResult>& classifyResults, c
         yaw_integration = 0.0;
     }
     chosen_result.command_pitch = last_pitch_rad_delayed_ + chosen_result.command_delta_pitch * command_picth_kp + pitch_integration; // PI控制
-    chosen_result.command_yaw = last_yaw_rad_delayed_ + chosen_result.command_delta_yaw * command_yaw_kp + yaw_integration; // 缓解yaw轴输入数据掉线问题（并不能()）
+    chosen_result.command_yaw = last_yaw_rad_delayed_ + chosen_result.command_delta_yaw * command_yaw_kp + yaw_integration + yaw_bias; // 缓解yaw轴输入数据掉线问题（并不能()）
 
 
     
