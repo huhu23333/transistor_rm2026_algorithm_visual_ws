54e16d53190abcd0935c7ea4489dff19a7d0ea11
huhu23333 <925393268@qq.com>
2026-03-11 14:48:37 +0800

    save change

--- src/auto_aim/include/3d_processing/BallisticSolver.h
+++ src/auto_aim/include/3d_processing/BallisticSolver.h
@@ -100,7 +100,7 @@ private:
         RefineInfo refine_info;
     };
 
-    float extra_delta_pitch = -1.0 * M_PI / 180.0f;
+    float extra_delta_pitch = 0.0 * M_PI / 180.0f;
     float extra_delta_yaw = -0.02;
 };
 #endif // BALLISTIC_SOLVER_H
