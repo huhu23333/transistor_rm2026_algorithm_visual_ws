2175c4e710945a8e7ad3a5fb56c8318bbb73dea6
huhu233 <925393268@qq.com>
2026-05-15 23:54:41 +0800

    增加openvino安装

--- pkgInstall/pkgInstall.bash
+++ pkgInstall/pkgInstall.bash
@@ -71,3 +71,10 @@ fi
 # 视频日志查看查看工具
 sudo apt install vlc
 
+cd ~/makeInstall
+wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB  
+sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB  
+echo "deb https://apt.repos.intel.com/openvino/2024 ubuntu22 main" | sudo tee /etc/apt/sources.list.d/intel-openvino-2024.list  
+sudo apt update 
+apt-cache search openvino
+sudo apt install openvino-2024.0.0
